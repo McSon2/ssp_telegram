d191099c3111c3597c1ea7816cf828b05b11ead3
McSon2 <maximesaltet@gmail.com>
2024-09-29 01:26:06 +0200

    modified:   .gitignore 	modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const bodyParser = require("body-parser");
+const Redis = require("ioredis");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,9 @@ const token = process.env.BOT_TOKEN;
 const url = process.env.APP_URL;
 
 const bot = new TelegramBot(token);
+
+// Configuration du client Redis
+const redis = new Redis(process.env.REDIS_URL);
 
 // Configuration du webhook avec gestion d'erreur
 async function setupWebhook() {
@@ -27,57 +31,53 @@ setupWebhook();
 
 // Gestion des mises à jour via webhook
 app.post(`/bot${token}`, (req, res) => {
-  console.log("Mise à jour reçue:", JSON.stringify(req.body));
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-// Map pour stocker les associations code -> chatId
-const userCodes = new Map();
-// Map pour suivre l'état de la conversation de chaque utilisateur
-const userStates = new Map();
-
 // Gestion de la commande /start
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
-  console.log(`Commande /start reçue du chat ID: ${chatId}`);
-  userStates.set(chatId, "WAITING_FOR_CODE");
+  await redis.set(`userState:${chatId}`, "WAITING_FOR_CODE");
   bot
     .sendMessage(
       chatId,
       "Bienvenue ! Veuillez entrer le code à 6 chiffres généré par l'application."
-    )
-    .then(() =>
-      console.log(`Message de bienvenue envoyé au chat ID: ${chatId}`)
     )
     .catch((error) =>
       console.error("Erreur lors de l'envoi du message de bienvenue:", error)
     );
 });
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
-  console.log(`Message reçu du chat ID ${chatId}: ${messageText}`);
 
   if (messageText === "/start") {
-    console.log("Commande /start ignorée car déjà gérée");
     return;
   }
 
-  const userState = userStates.get(chatId);
-  console.log(`État actuel de l'utilisateur ${chatId}: ${userState}`);
+  const userState = await redis.get(`userState:${chatId}`);
 
   if (userState === "WAITING_FOR_CODE") {
     if (messageText && messageText.length === 6 && /^\d+$/.test(messageText)) {
-      userCodes.set(messageText, chatId);
-      userStates.set(chatId, "CODE_VALIDATED");
+      // Vérifier si un code précédent est associé à ce chatId
+      const oldCode = await redis.get(`chatId:${chatId}`);
+      if (oldCode) {
+        // Supprimer l'ancien code
+        await redis.del(`code:${oldCode}`);
+      }
+
+      // Enregistrer le nouveau code
+      await redis.set(`code:${messageText}`, chatId);
+      await redis.set(`chatId:${chatId}`, messageText);
+      await redis.set(`userState:${chatId}`, "CODE_VALIDATED");
+
       bot
         .sendMessage(
           chatId,
           "Code validé ! Vous recevrez maintenant des notifications."
         )
-        .then(() => console.log(`Code validé pour le chat ID: ${chatId}`))
         .catch((error) =>
           console.error(
             "Erreur lors de l'envoi du message de validation:",
@@ -90,9 +90,6 @@ bot.on("message", (msg) => {
           chatId,
           "Le code doit être composé de 6 chiffres. Veuillez réessayer."
         )
-        .then(() =>
-          console.log(`Message d'erreur envoyé au chat ID: ${chatId}`)
-        )
         .catch((error) =>
           console.error("Erreur lors de l'envoi du message d'erreur:", error)
         );
@@ -103,7 +100,6 @@ bot.on("message", (msg) => {
         chatId,
         "Pour commencer, veuillez utiliser la commande /start."
       )
-      .then(() => console.log(`Instructions envoyées au chat ID: ${chatId}`))
       .catch((error) =>
         console.error(
           "Erreur lors de l'envoi du message d'instructions:",
@@ -113,18 +109,14 @@ bot.on("message", (msg) => {
   }
 });
 
-app.post("/send-notification", (req, res) => {
+app.post("/send-notification", async (req, res) => {
   const { code, message } = req.body;
-  console.log(
-    `Tentative d'envoi de notification. Code: ${code}, Message: ${message}`
-  );
 
-  if (userCodes.has(code)) {
-    const chatId = userCodes.get(code);
+  const chatId = await redis.get(`code:${code}`);
+  if (chatId) {
     bot
       .sendMessage(chatId, message)
       .then(() => {
-        console.log(`Notification envoyée avec succès au chat ID: ${chatId}`);
         res.status(200).send("Notification envoyée avec succès");
       })
       .catch((error) => {
